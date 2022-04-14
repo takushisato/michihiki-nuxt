5341950eb3b3f52a66e1ea9788c929ca1424317f
takushisato <takuchan19820801@yahoo.co.jp>
2022-04-14 17:08:56 +0900

    タイド計算完。分解前

---
+++ components/TideCalculation.vue
@@ -0,0 +1,19 @@
+<script>
+import { timeDatas } from '~/datas/timeData.js'
+import index from '~/pages/index.vue'
+
+export default {
+    data(){
+        return{
+            index: index,
+            timeDatas: timeDatas
+        }
+    },
+    methods: {
+        test:function(){
+            alert('hiraita!!');
+        }
+
+    }
+}
+</script>
