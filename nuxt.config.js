1ae0499c433f72c4a4b2dcc709dcc05d0c6d8a6f
takushisato <takuchan19820801@yahoo.co.jp>
2022-04-23 13:41:46 +0900

    API設定変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -71,5 +71,11 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  mode: 'spa',
+  target: 'server',
+  generate: {
+    fallback: true
   }
 }
