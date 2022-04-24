67e101a910826ceb51196c4e1172eafd0480cf79
takushisato <takuchan19820801@yahoo.co.jp>
2022-04-25 08:51:44 +0900

    エラー修正

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,3 @@
-const baseApiUrl = process.env.BASE_API_URL || "http://localhost";
-const baseImgUrl = process.env.BASE_IMG_URL || "http://localhost";
-
 export default {
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
   ssr: false,
@@ -60,11 +57,11 @@ export default {
 
   proxy: {
     '/api/': {
-      target: baseApiUrl,
+      target: 'https://api.tide736.net/get_tide.php',
       pathRewrite: {'^/api/': ''}
     },
     '/img-api/': {
-      target: baseImgUrl,
+      target: 'https://api.tide736.net/tide_image.php',
       pathRewrite: {'^/img-api/': ''}
     },
   },
