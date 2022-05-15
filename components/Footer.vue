36e9a50ff5ada1a06d9ad795fa08752509095640
takushisato <takuchan19820801@yahoo.co.jp>
2022-05-15 20:20:34 +0900

    アドセンス広告追加

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="select-none">
         <div class="footer-twitter text-center block pb-4 text-lg">
           <br>
-          <nuxt-link to="/contact" class="text-xl hover:opacity-30">お問い合わせはこちら</nuxt-link>
+          <nuxt-link to="/contact" class="text-lg hover:opacity-30">お問い合わせはこちら</nuxt-link>
           <h1 class="pt-5">他サイトのリンク</h1>
           <h2>（どちらもヤフー天気予報へのリンクです。）</h2>
           <button class="block text-center m-auto"><a href="https://weather.yahoo.co.jp/weather/wind/?m=ground" class="block hover:opacity-30" target=”_blank”>風予報</a></button>
--- pages/precautions.vue
+++ pages/precautions.vue
@@ -1,16 +1,16 @@
 <template>
 <div>
    <Header />
-   <div class="container m-auto text-left text-lg">
+   <div class="container m-auto text-left">
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">注意事項</h1>
-         <h2 class="text-1xl">当サイトについて</h2>
+         <h1 class="underline">注意事項</h1>
+         <h2>当サイトについて</h2>
          <p>当サイトをご利用いただき誠にありがとうございます。当サイトを快適かつ適切にご利用いただくため、下記内容をご一読くださいますようお願いいたします。</p>
       </div>
 
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">Webブラウザなど閲覧環境</h1>
-         <h2 class="text-1xl">当サイトの推奨環境は以下の通りです。</h2>
+         <h1 class="underline">Webブラウザなど閲覧環境</h1>
+         <h2>当サイトの推奨環境は以下の通りです。</h2>
          <p>Windows edge、Chrome、Firefox</p>
          <p>Mac OS Safari</p>
          <p>iPhone iOS、Android</p>
@@ -19,33 +19,31 @@
       </div>
 
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">著作権について</h1>
+         <h1 class="underline">著作権について</h1>
          <p>当サイトに掲載されているコンテンツ内容は、海上保安庁が公開している『潮汐表APIサービス』からデータを引用させていただき作成しております。（記載内容の著作権などは海上保安庁に帰属します。）</p>
          <p>また、当サイトに掲載されているコンテンツ（文章、画像、写真、動画、ロゴ、プログラム等）を無断で転用・複製などをすることは出来ません。</p>
          <a href="https://tide736.net/" class="hover:opacity-30" target=”_blank”>引用元：海上保安庁 潮汐表APIサービス</a>
       </div>
 
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">外部サイトへのリンクについて</h1>
+         <h1 class="underline">外部サイトへのリンクについて</h1>
          <p>当サイトには、外部のウェブサイトへのリンク（API）が含まれています。リンク先の外部サイトは、各サイト運営者の責任において管理運営されており、外部サイトは当サイトの管理下にありません。</p>
          <p>当サイトは、お客様が外部サイトをご利用になったことにより生じたいかなる損害についても責任を負いかねますので、予めご了承ください。</p>
       </div>
 
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">当サイトへのリンクの注意事項</h1>
+         <h1 class="underline">当サイトへのリンクの注意事項</h1>
          <p>1,当サイトの有する権利を侵害または妨害する行為、また当サイトのイメージを損なうようなリンクを設定することは固く禁じます。</p>
          <p>2,フレーム内での表示等、当サイトへのリンクとなることが不明瞭になる方法でのリンク設定はご遠慮ください。</p>
       </div>
 
       <div class="m-5 mt-10">
-         <h1 class="text-2xl underline">その他</h1>
+         <h1 class="underline">その他</h1>
          <p>サイトのバグ、システムの不具合、記載内容に関して何か問題がありましたら制作者にご一報頂けると幸いです。</p>
          <br>
          <nuxt-link to="/contact" class="hover:opacity-30">お問い合わせフォーム</nuxt-link>
       </div>
-
-      <nuxt-link to=""></nuxt-link>
-
+      
       <div class="mt-20 mb-20">
          <img src="~/static/logo.png" alt="ロゴ画像" class="block m-auto">
       </div>
