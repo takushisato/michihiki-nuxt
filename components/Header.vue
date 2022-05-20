47465633b58383e14facfb359b5f692dd0642055
takushisato <takuchan19820801@yahoo.co.jp>
2022-05-20 21:55:06 +0900

    レイアウト崩れ（1440px）修正

--- components/Header.vue
+++ components/Header.vue
@@ -3,13 +3,11 @@
     <header class="select-none">
     <div class="title">
     <h2 class="text-1xl">グラフで確認する潮見表サイト</h2>
-    <nuxt-link to="/"><h1 class="text-5xl hover:opacity-30">みちひき</h1></nuxt-link>
+    <a href="/"><h1 class="text-5xl hover:opacity-30">みちひき</h1></a>
     </div>
     </header>
     <div class="bg-blue-800 text-center text-white text-lg">
       <nuxt-link to="/precautions" class="hover:opacity-30">注意事項</nuxt-link>
     </div>
-    <!-- <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4568664831429987"
-     crossorigin="anonymous"></script></head> -->
 </div>
 </template>
